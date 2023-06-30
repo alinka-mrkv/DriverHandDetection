5cf3996bba3d7c5901609fa42cda6a88ef2c3c19
Lada <lada.pestova2004@gmail.com>
2023-06-30 15:59:21 +0300

    fix: removed comments and changed code structure for readability

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Импортируем необходимые модули.
 import argparse
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
@@ -9,7 +8,6 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
 
-# Добавляем поддержку аргументов командной строки.
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Need to enter video file path')
     parser.add_argument('-v', '--video', type=str, required=True, help='Path to video file')
@@ -18,15 +16,25 @@ def parse_arguments():
     return args.video
 
 
+def create_detector():
+    BaseOptions = mp.tasks.BaseOptions
+    VisionRunningMode = mp.tasks.vision.RunningMode
+    model_path = 'pose_landmarker_heavy.task'
+    options = vision.PoseLandmarkerOptions(
+        base_options=BaseOptions(model_asset_path=model_path),
+        running_mode=VisionRunningMode.VIDEO,
+    )
+    pose_detector = vision.PoseLandmarker.create_from_options(options)
+    return pose_detector
+
+
 def annotate_image_with_landmarks(rgb_image, pose_detections):
     pose_landmarks_list = pose_detections.pose_landmarks
     annotated_image = np.copy(rgb_image)
 
-    # Обходим обнаруженные позы для визуализации.
     for idx in range(len(pose_landmarks_list)):
         pose_landmarks = pose_landmarks_list[idx]
 
-        # Отображаем ориентиры позы.
         pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
         pose_landmarks_proto.landmark.extend([
             landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
@@ -39,21 +47,8 @@ def annotate_image_with_landmarks(rgb_image, pose_detections):
     return annotated_image
 
 
-if __name__ == '__main__':
-    model_path = 'pose_landmarker_heavy.task'
-    BaseOptions = mp.tasks.BaseOptions
-    ImageClassifier = mp.tasks.vision.ImageClassifier
-    VisionRunningMode = mp.tasks.vision.RunningMode
-
-    # Создаем объект PoseLandmarker.
-    options = vision.PoseLandmarkerOptions(
-        base_options=BaseOptions(model_asset_path=model_path),
-        running_mode=VisionRunningMode.VIDEO,
-    )
-    pose_detector = vision.PoseLandmarker.create_from_options(options)
-    video_path = parse_arguments()
-    # Если указан путь к видеофайлу, то читаем его.
-    if video_path:
+def process_video_with_landmarks(path):
+    if path:
         cap = cv2.VideoCapture(video_path)
 
         if not cap.isOpened():
@@ -61,27 +56,32 @@ if __name__ == '__main__':
             exit(-1)
 
         video_file_fps = cap.get(cv2.CAP_PROP_FPS)
-
-        frame_count = 0  # initialize a frame counter
+        frame_count = 0
 
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
 
-            # calculate frame timestamp in milliseconds
             frame_timestamp_ms = int(1000 * frame_count / video_file_fps)
 
-            # cv2.imshow('Frame', frame)
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-            pose_landmarker_result = pose_detector.detect_for_video(mp_image, frame_timestamp_ms)
+            pose_landmarker_result = detector.detect_for_video(mp_image, frame_timestamp_ms)
             annotated_image = annotate_image_with_landmarks(frame, pose_landmarker_result)
             cv2.imshow('annotated_image', annotated_image)
 
-            if cv2.waitKey(1) & 0xFF == 27:  # Изменение условия сравнения
+            if cv2.waitKey(1) & 0xFF == 27:
                 break
 
-            frame_count += 1  # increment the frame counter
+            frame_count += 1
 
         cap.release()
         cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+
+    detector = create_detector()
+    video_path = parse_arguments()
+    process_video_with_landmarks(video_path)
+
