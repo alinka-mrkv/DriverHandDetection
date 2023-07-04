76267cbe9ad918d7b5e9b9a9cc429a184ac6a1f7
EzikSu <ioneion200@gmail.ru>
2023-07-04 12:54:59 +0300

    add: added gui_p1.py which will be integrated into main.py to provide basic graphic user interface

---
+++ gui_p1.py
@@ -0,0 +1,148 @@
+import argparse
+import cv2
+import numpy as np
+
+
+class VideoGUI:
+    def __init__(self, video_path):
+        self.video_path = video_path
+        self.cap = None
+        self.frame_count = 0
+        self.frame = None
+        self.cursor_positions = []
+        self.saved_convex_shape = []
+        self.saved = False
+        self.ROI_MODE = False
+        self.DET_MODE = False
+        self.frame_copy = None
+
+    def init_video_gui(self):
+        self.cap = cv2.VideoCapture(self.video_path)
+
+        if not self.cap.isOpened():
+            print("Error opening video file.")
+            exit(-1)
+
+        cv2.namedWindow("GUI")
+        cv2.setMouseCallback("GUI", self.mouse_callback)
+
+        self.frame_count = 0
+        self.frame = None
+
+    def terminate_video_gui(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+    def mouse_callback(self, event, x, y, flags, param):
+        if self.ROI_MODE:
+            if event == cv2.EVENT_LBUTTONDOWN:
+                if len(self.cursor_positions) == 10:
+                    self.cursor_positions.pop(0)
+                self.cursor_positions.append((x, y))
+            elif event == cv2.EVENT_RBUTTONDOWN and self.cursor_positions:
+                self.cursor_positions.pop()
+
+    def key_handler(self):
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == 32:
+            self.ROI_MODE = not self.ROI_MODE
+            self.DET_MODE = False
+            if self.ROI_MODE:
+                print("ROI_MODE enabled!")
+        elif key == 13:
+            if is_convex(self.cursor_positions):
+                if self.ROI_MODE:
+                    print("Convex shape saved successfully!")
+                    self.saved_convex_shape = self.cursor_positions
+                    self.saved = True
+                    self.ROI_MODE = False
+                    self.DET_MODE = not self.DET_MODE
+                    if self.DET_MODE:
+                        print("DET_MODE enabled!")
+        elif key == 27:
+            print("User chose to exit.")
+            return "exit"
+
+    def draw_positions(self, current_frame):
+        for pos in self.cursor_positions:
+            cv2.circle(current_frame, pos, 5, (255, 0, 0), -1)
+
+    def draw_connections(self, current_frame):
+        if len(self.cursor_positions) > 1:
+            for i in range(len(self.cursor_positions) - 1):
+                cv2.line(current_frame, self.cursor_positions[i], self.cursor_positions[i + 1], (212, 255, 127), 2)
+            if len(self.cursor_positions) > 2:
+                cv2.line(current_frame, self.cursor_positions[-1], self.cursor_positions[0], (212, 255, 127), 2)
+
+    def draw_roi(self, current_frame):
+        self.frame_copy = current_frame.copy()
+        self.draw_positions(self.frame_copy)
+        self.draw_connections(self.frame_copy)
+        if not is_convex(self.cursor_positions):
+            draw_text_on_frame(self.frame_copy, "Shape must be convex to save!", 300, 160)
+        return self.frame_copy
+
+    def roi_mode(self):
+        self.frame_copy = self.draw_roi(self.frame)
+        cv2.imshow("GUI", self.frame_copy)
+
+    def play_mode(self):
+        ret, self.frame = self.cap.read()
+        if not ret:
+            print("Error reading video frame.")
+            exit(-1)
+        cv2.imshow("GUI", self.frame)
+        self.frame_count += 1
+
+
+def draw_text_on_frame(current_frame, text, x, y):
+    cv2.putText(
+        current_frame,
+        text,
+        (x, y),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1.5,
+        (0, 0, 255),
+        2,
+    )
+
+
+def is_convex(positions):
+    if len(positions) > 2:
+        return cv2.isContourConvex(np.array(positions))
+    else:
+        return False
+
+
+def parse_argument():
+    parser = argparse.ArgumentParser(description="Need to enter video file path")
+    parser.add_argument("-v", "--video", type=str, required=True, help="Path to video file")
+
+    args = parser.parse_args()
+    return args.video
+
+
+def det_mode():
+    image = np.empty((720, 1280, 3), dtype=np.uint8)
+    draw_text_on_frame(image, "detection mode not implemented!", 260, 120)
+    cv2.imshow("GUI", image)
+
+
+def run_video_gui(gui):
+    gui.init_video_gui()
+    while gui.key_handler() != "exit":
+        if gui.ROI_MODE:
+            gui.roi_mode()
+        elif gui.DET_MODE:
+            det_mode()
+        else:
+            gui.play_mode()
+
+    gui.terminate_video_gui()
+
+
+if __name__ == "__main__":
+    video_path = parse_argument()
+    gui = VideoGUI(video_path)
+    run_video_gui(gui)
