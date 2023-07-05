c234ce8a8bb19a002c6fdb7be41798a1347311ae
EzikSu <ioneion200@gmail.ru>
2023-07-05 15:41:26 +0300

    test: modified test_gui_p1.py to provide better code coverage

--- test_gui_p1.py
+++ test_gui_p1.py
@@ -40,6 +40,13 @@ def test_mouse_callback_delete_position(gui_p1):
     assert gui_p1.cursor_positions == []
 
 
+def test_mouse_callback_add_position_if_10_exist(gui_p1):
+    gui_p1.ROI_MODE = True
+    gui_p1.cursor_positions = [(200, 300), (120, 210), (100, 200), (100, 200), (100, 200), (100, 200), (100, 200), (100, 200), (100, 200), (100, 200), ]
+    gui_p1.mouse_callback(cv2.EVENT_LBUTTONDOWN, 150, 250, 0, 0)
+    assert gui_p1.cursor_positions == [(120, 210), (100, 200), (100, 200), (100, 200), (100, 200), (100, 200), (100, 200), (100, 200), (100, 200), (150, 250)]
+
+
 def test_key_handler_roi_mode_toggle(gui_p1):
     gui_p1.ROI_MODE = False
     cv2.waitKey = lambda _: 32
@@ -139,6 +146,56 @@ def test_draw_connections_three_positions(gui_p1):
     assert np.array_equal(cur_frame, expected_frame)
 
 
+@pytest.fixture
+def circles_and_lines1():
+    expected_frame = np.zeros((480, 640, 3), dtype=np.uint8)
+    cv2.circle(expected_frame, (100, 100), 5, (255, 0, 0), -1)
+    cv2.circle(expected_frame, (200, 100), 5, (255, 0, 0), -1)
+    cv2.circle(expected_frame, (200, 200), 5, (255, 0, 0), -1)
+    cv2.circle(expected_frame, (100, 200), 5, (255, 0, 0), -1)
+    cv2.line(expected_frame, (100, 100), (200, 100), (212, 255, 127), 2)
+    cv2.line(expected_frame, (200, 100), (200, 200), (212, 255, 127), 2)
+    cv2.line(expected_frame, (200, 200), (100, 200), (212, 255, 127), 2)
+    cv2.line(expected_frame, (100, 200), (100, 100), (212, 255, 127), 2)
+    return expected_frame
+
+
+def test_draw_roi_is_convex(gui_p1, circles_and_lines1):
+    current_frame = np.zeros((480, 640, 3), dtype=np.uint8)
+    gui_p1.cursor_positions = [(100, 100), (200, 100), (200, 200), (100, 200)]
+    output_frame = gui_p1.draw_roi(current_frame)
+    assert np.array_equal(output_frame, circles_and_lines1)
+
+
+@pytest.fixture
+def circles_and_lines2():
+    expected_frame = np.zeros((480, 640, 3), dtype=np.uint8)
+    cv2.circle(expected_frame, (100, 100), 5, (255, 0, 0), -1)
+    cv2.circle(expected_frame, (100, 120), 5, (255, 0, 0), -1)
+    cv2.circle(expected_frame, (200, 200), 5, (255, 0, 0), -1)
+    cv2.circle(expected_frame, (100, 200), 5, (255, 0, 0), -1)
+    cv2.line(expected_frame, (100, 100), (100, 120), (212, 255, 127), 2)
+    cv2.line(expected_frame, (100, 120), (200, 200), (212, 255, 127), 2)
+    cv2.line(expected_frame, (200, 200), (100, 200), (212, 255, 127), 2)
+    cv2.line(expected_frame, (100, 200), (100, 100), (212, 255, 127), 2)
+    draw_text_on_frame(expected_frame, "Shape must be convex to save!", 300, 160)
+    return expected_frame
+
+
+def test_draw_roi_not_is_convex(gui_p1, circles_and_lines2):
+    current_frame = np.zeros((480, 640, 3), dtype=np.uint8)
+    gui_p1.cursor_positions = [(100, 100), (100, 120), (200, 200), (100, 200)]
+    output_frame = gui_p1.draw_roi(current_frame)
+    assert np.array_equal(output_frame, circles_and_lines2)
+
+
+def test_roi_mode(gui_p1, circles_and_lines2):
+    gui_p1.frame = np.zeros((480, 640, 3), dtype=np.uint8)
+    gui_p1.cursor_positions = [(100, 100), (100, 120), (200, 200), (100, 200)]
+    gui_p1.roi_mode()
+    assert np.array_equal(gui_p1.frame_copy, circles_and_lines2)
+
+
 def test_is_convex_case1():
     positions1 = [(100, 100), (200, 200), (300, 100)]
     result1 = is_convex(positions1)
@@ -168,6 +225,15 @@ def test_play_mode(gui_p1, mock_video_capture):
     assert gui_p1.frame_count == 1
 
 
+def test_play_mode_exit(gui_p1):
+    gui_p1.frame = None
+    gui_p1.frame_count = 0
+    gui_p1.cap = cv2.VideoCapture("12345qwer12312zfcdasdadfdsfsdfsdfsdtrew")
+
+    with pytest.raises(SystemExit):
+        gui_p1.play_mode()
+
+
 def test_init_video_gui(gui_p1):
     gui_p1.init_video_gui()
 
@@ -177,6 +243,16 @@ def test_init_video_gui(gui_p1):
     assert gui_p1.frame_count == 0
     assert gui_p1.frame is None
     gui_p1.terminate_video_gui()
+
+
+def test_init_video_gui_exit(gui_p1, capsys):
+    gui_p1.video_path = "abczyx123456qwerty"
+    with pytest.raises(SystemExit) as exc_info:
+        gui_p1.init_video_gui()
+
+    captured = capsys.readouterr()
+    assert exc_info.value.code == -1
+    assert "Error opening video file." in captured.out
 
 
 def test_terminate_video_gui(gui_p1):
